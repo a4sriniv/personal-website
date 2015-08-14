0633f0651464adb7f2434b68dc70d232d203abfb
Adithya <a4sriniv@uwaterloo.ca>
2015-08-14 16:25:25 -0400

    Add page link handler for resume page

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ class MainPage(webapp2.RequestHandler):
         with open(os.path.join(os.getcwd(), 'index.html')) as fin:
             self.response.write(fin.read())
 
+class Resume(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        with open(os.path.join(os.getcwd(), 'resume.html')) as fin:
+            self.response.write(fin.read())
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/resume', Resume),
 ], debug=False)
